c1fef957af60815a3257906c8169a29439a8e144
Sarah <sarah@efficientera.com>
2018-08-06 14:15:27 -0700

    Fixed table sequences when cloning schema

--- django_tenants/utils.py
+++ django_tenants/utils.py
@@ -179,8 +179,14 @@ DECLARE
   src_oid          oid;
   tbl_oid          oid;
   func_oid         oid;
+  table_rec        record;
+  seq_rec          record;
   object           text;
+  sequence_        text;
+  table_           text;
   buffer           text;
+  seq_buffer       text;
+  table_buffer     text;
   srctbl           text;
   default_         text;
   column_          text;
@@ -206,98 +212,47 @@ BEGIN
     FROM pg_namespace
    WHERE nspname = source_schema;
   IF NOT FOUND
-    THEN 
+    THEN
     RAISE EXCEPTION 'source schema % does not exist!', source_schema;
     RETURN ;
   END IF;
 
-  -- Check that dest_schema does not yet exist
-  PERFORM nspname 
+-- Check that dest_schema does not yet exist
+  PERFORM nspname
     FROM pg_namespace
    WHERE nspname = dest_schema;
   IF FOUND
-    THEN 
+    THEN
     RAISE EXCEPTION 'dest schema % already exists!', dest_schema;
     RETURN ;
   END IF;
 
   EXECUTE 'CREATE SCHEMA "' || dest_schema || '"';
 
-  -- Create sequences
-  -- TODO: Find a way to make this sequence's owner is the correct table.
+-- Create tables
   FOR object IN
-    SELECT sequence_name::text 
-      FROM information_schema.sequences
-     WHERE sequence_schema = source_schema
-  LOOP
-    EXECUTE 'CREATE SEQUENCE "' || dest_schema || '".' || quote_ident(object);
-    srctbl := '"' || source_schema || '".' || quote_ident(object);
-
-    EXECUTE 'SELECT last_value, max_value, start_value, increment_by, min_value, cache_value, log_cnt, is_cycled, is_called 
-              FROM "' || source_schema || '".' || quote_ident(object) || ';' 
-              INTO sq_last_value, sq_max_value, sq_start_value, sq_increment_by, sq_min_value, sq_cache_value, sq_log_cnt, sq_is_cycled, sq_is_called ; 
-
-    IF sq_is_cycled 
-      THEN 
-        sq_cycled := 'CYCLE';
-    ELSE
-        sq_cycled := 'NO CYCLE';
-    END IF;
-
-    EXECUTE 'ALTER SEQUENCE "'   || dest_schema || '".' || quote_ident(object) 
-            || ' INCREMENT BY ' || sq_increment_by
-            || ' MINVALUE '     || sq_min_value 
-            || ' MAXVALUE '     || sq_max_value
-            || ' START WITH '   || sq_start_value
-            || ' RESTART '      || sq_min_value 
-            || ' CACHE '        || sq_cache_value 
-            || sq_cycled || ' ;' ;
-
-    buffer := '"' || dest_schema || '".' || quote_ident(object);
-    IF include_recs 
-        THEN
-            EXECUTE 'SELECT setval( ''' || buffer || ''', ' || sq_last_value || ', ' || sq_is_called || ');' ; 
-    ELSE
-            EXECUTE 'SELECT setval( ''' || buffer || ''', ' || sq_start_value || ', ' || sq_is_called || ');' ;
-    END IF;
-
-  END LOOP;
-
--- Create tables 
-  FOR object IN
-    SELECT TABLE_NAME::text 
-      FROM information_schema.tables 
+    SELECT TABLE_NAME::text
+      FROM information_schema.tables
      WHERE table_schema = source_schema
        AND table_type = 'BASE TABLE'
 
   LOOP
     buffer := '"' || dest_schema || '".' || quote_ident(object);
-    EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE "' || source_schema || '".' || quote_ident(object) 
-        || ' INCLUDING ALL)';
+    EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE "' || source_schema || '".' || quote_ident(object)
+        || ' INCLUDING ALL);';
 
-    IF include_recs 
-      THEN 
+    IF include_recs
+      THEN
       -- Insert records from source table
       EXECUTE 'INSERT INTO ' || buffer || ' SELECT * FROM "' || source_schema || '".' || quote_ident(object) || ';';
     END IF;
-
-    FOR column_, default_ IN
-      SELECT column_name::text, 
-             REPLACE(column_default::text, source_schema, dest_schema) 
-        FROM information_schema.COLUMNS 
-       WHERE table_schema = dest_schema 
-         AND TABLE_NAME = object 
-         AND column_default LIKE 'nextval(%"' || source_schema || '"%::regclass)'
-    LOOP
-      EXECUTE 'ALTER TABLE ' || buffer || ' ALTER COLUMN ' || column_ || ' SET DEFAULT ' || default_;
-    END LOOP;
 
   END LOOP;
 
 --  add FK constraint
   FOR qry IN
-    SELECT 'ALTER TABLE "' || dest_schema || '".' || quote_ident(rn.relname) 
-                          || ' ADD CONSTRAINT ' || quote_ident(ct.conname) || ' ' || pg_get_constraintdef(ct.oid) || ';'
+    SELECT 'ALTER TABLE "' || dest_schema || '".' || quote_ident(rn.relname)
+            || ' ADD CONSTRAINT ' || quote_ident(ct.conname) || ' ' || pg_get_constraintdef(ct.oid) || ';'
       FROM pg_constraint ct
       JOIN pg_class rn ON rn.oid = ct.conrelid
      WHERE connamespace = src_oid
@@ -309,11 +264,78 @@ BEGIN
 
     END LOOP;
 
+-- Create sequences
+  FOR seq_rec IN
+    SELECT
+      s.sequence_name::text,
+      table_name,
+      column_name
+    FROM information_schema.sequences s
+    JOIN (
+      SELECT
+        substring(column_default from E'^nextval\\\\(''(?:[^"\'\']?.*["\'\']?\\\\.)?([^\'\']*)\'\'(?:::text|::regclass)?\\\\)')::text AS seq_name,
+        table_name,
+        column_name
+      FROM information_schema.columns
+      WHERE column_default LIKE 'nextval%'
+        AND table_schema = source_schema
+    ) c ON c.seq_name = s.sequence_name
+    WHERE sequence_schema = source_schema
+  LOOP
+    seq_buffer := quote_ident(dest_schema) || '.' || quote_ident(seq_rec.sequence_name);
 
--- Create views 
+    RAISE NOTICE 'seq buffer %', seq_buffer;
+    EXECUTE 'CREATE SEQUENCE ' || seq_buffer || ';';
+
+    qry := 'SELECT last_value, max_value, start_value, increment_by, min_value, cache_value, log_cnt, is_cycled, is_called
+              FROM "' || source_schema || '".' || quote_ident(seq_rec.sequence_name) || ';';
+    EXECUTE qry INTO sq_last_value, sq_max_value, sq_start_value, sq_increment_by, sq_min_value, sq_cache_value, sq_log_cnt, sq_is_cycled, sq_is_called ;
+
+    IF sq_is_cycled
+      THEN
+        sq_cycled := 'CYCLE';
+    ELSE
+        sq_cycled := 'NO CYCLE';
+    END IF;
+
+    EXECUTE 'ALTER SEQUENCE '   || seq_buffer
+            || ' INCREMENT BY ' || sq_increment_by
+            || ' MINVALUE '     || sq_min_value
+            || ' MAXVALUE '     || sq_max_value
+            || ' START WITH '   || sq_start_value
+            || ' RESTART '      || sq_min_value
+            || ' CACHE '        || sq_cache_value
+            || ' OWNED BY '     || quote_ident(dest_schema ) || '.'
+                                || quote_ident(seq_rec.table_name) || '.'
+                                || quote_ident(seq_rec.column_name) || ' '
+            || sq_cycled || ' ;' ;
+
+    IF include_recs
+        THEN
+            EXECUTE 'SELECT setval(' || quote_literal(seq_buffer) || ', ' || sq_last_value || ', ' || sq_is_called || ');' ;
+    ELSE
+            EXECUTE 'SELECT setval(' || quote_literal(seq_buffer) || ', ' || sq_start_value || ', ' || sq_is_called || ');' ;
+    END IF;
+
+    table_buffer := quote_ident(dest_schema) || '.' || quote_ident(seq_rec.table_name);
+
+    FOR table_rec IN
+      SELECT column_name::text AS column_,
+             REPLACE(column_default::text, source_schema, quote_ident(dest_schema)) AS default_
+        FROM information_schema.COLUMNS
+       WHERE table_schema = dest_schema
+         AND TABLE_NAME = seq_rec.table_name
+         AND column_default LIKE 'nextval(%' || seq_rec.sequence_name || '%::regclass)'
+    LOOP
+      EXECUTE 'ALTER TABLE ' || table_buffer || ' ALTER COLUMN ' || table_rec.column_ || ' SET DEFAULT nextval(' || quote_literal(seq_buffer) || '::regclass);';
+    END LOOP;
+
+  END LOOP;
+
+-- Create views
   FOR object IN
     SELECT table_name::text,
-           view_definition 
+           view_definition
       FROM information_schema.views
      WHERE table_schema = source_schema
 
@@ -328,20 +350,20 @@ BEGIN
 
   END LOOP;
 
--- Create functions 
+-- Create functions
   FOR func_oid IN
     SELECT oid
-      FROM pg_proc 
+      FROM pg_proc
      WHERE pronamespace = src_oid
 
-  LOOP      
+  LOOP
     SELECT pg_get_functiondef(func_oid) INTO qry;
     SELECT replace(qry, source_schema, dest_schema) INTO dest_qry;
     EXECUTE dest_qry;
 
   END LOOP;
 
-  RETURN; 
+  RETURN;
 
 END;
 
